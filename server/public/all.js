d4f1f7447fbca57b71d120c54cd9a4ed23d14139
Yogibuff <yogibuff@icloud.com>
2016-01-29 13:08:22 -0800

    #2 Create Account button now functional. Generates a modal where users can login, create a new account, and recover lost passwords.

--- server/public/all.js
+++ server/public/all.js
@@ -13,6 +13,81 @@ angular.module('legacyCrowdApp', []);
 //   }]));
 // existing legacyCrowdApp module
 var legacy = angular.module('legacyCrowdApp');
+
+/* add createCampaign controller with logic for manipulating the campaign creator UI
+and handling user input campaign data within the $scope */
+legacy.controller('campaignCreatorCtrl', function($scope) {
+  $scope.helloWorld = function() {
+    console.log('testing - helloWorld function within the campaignCreatorCtrl');
+  };
+});
+
+legacy.directive('goal', function() {
+  return {
+    template:
+  '<h1>Start Building Your Project!</h1>' +
+  '<br>' +
+  '<h4>Add an Image, Set a Goal, and include Important Details</h4>' +
+  '<br>'
+  };
+});
+
+legacy.directive('build', function() {
+  return {
+    template:
+  '<div id="build-campaign">' +
+    '<h1>Start Building Your Project!</h1>' +
+    '<br>' +
+    '<h4>Add an Image, Set a Goal, and include Important Details</h4>' +
+    '<br>' +
+      '<form novalidate>' +
+        '<ul>' +
+          '<li>' +
+            '<label class="build-input">Project Image:</label><input ng-model="creator.imageUpload">' +
+                 '{{ creator.imageUpload }}' +
+          '</li>' +
+          '<li>' +
+            '<label class="build-input">Funding Target:</label><input type="text" ng-model="creator.target">' +
+             '{{ creator.target }}' +
+          '</li>' +
+          '<li>' +
+            '<label class="build-input">Project Title:</label><input ng-model="creator.title">' +
+              '{{ creator.title }}' +
+          '</li>' +
+          '<li>' +
+            '<label class="build-input">Short Blurb:</label><input type="text" ng-model="creator.blurb">' +
+              '{{ creator.blurb }}' +
+          '</li>' +
+          '<li>' +
+            '<label class="build-input">Location:</label><input ng-model="creator.location">' +
+              '{{ creator.location }}' +
+          '</li>' +
+          '<li>' +
+            '<label class="build-input">Category:</label><input type="text" ng-model="creator.category">' +
+              '{{ creator.category }}' +
+          '</li>' +
+          '<li>' +
+            '<label class="build-input">Campaign Start Date:</label><input ng-model="creator.startDate">' +
+              '{{ creator.startDate }}' +
+          '</li>' +
+          '<li>' +
+            '<label class="build-input">Campaign End Date:</label><input type="text" ng-model="creator.endDate">' +
+              '{{ creator.endDate }}' +
+          '</li>' +
+        '</ul>' +
+      '</form>' +
+    '</div> <!-- close #build-campaign -->'
+  };
+});
+
+// test service, creates the method testingMyService
+legacy.service('data-service', function() {
+  this.testingMyService(function() {
+    console.log('This is my service!');
+  });
+});
+// existing legacyCrowdApp module
+var legacy = angular.module('legacyCrowdApp');
 legacy.controller('featuredCampaignCtrl', function() {
   vm = this;
   // add logic and variables
@@ -55,82 +130,6 @@ legacy.directive('topCampaigns', function() {
       ' ' + '<div featured-cards>' + '</div>' + ' ' + '<div featured-cards>' + '</div>' + ' ' + '</div>'
   };
 });
-// existing legacyCrowdApp module
-var legacy = angular.module('legacyCrowdApp');
-
-/* add createCampaign controller with logic for manipulating the campaign creator UI
-and handling user input campaign data within the $scope */
-legacy.controller('campaignCreatorCtrl', function($scope) {
-  $scope.helloWorld = function() {
-    console.log('testing - helloWorld function within the campaignCreatorCtrl');
-  };
-});
-
-legacy.directive('goal', function() {
-  return {
-    template: '<h1>Start Building Your Project!</h1>' +
-  '<br>' +
-  '<h4>Add an Image, Set a Goal, and include Important Details</h4>' +
-  '<br>'
-  };
-});
-
-legacy.directive('build', function() {
-  return {
-    template:
-'<div id="create">' +
-  '<div ng-controller="campaignCreatorCtrl as creator">' +
-  '<h1>Start Building Your Project!</h1>' +
-  '<br>' +
-  '<h4>Add an Image, Set a Goal, and include Important Details</h4>' +
-  '<br>' +
-    '<form novalidate>' +
-      '<ul>' +
-        '<li>' +
-          '<label class="build-input">Project Image:</label><input ng-model="creator.imageUpload">' +
-               '{{ creator.imageUpload }}' +
-        '</li>' +
-        '<li>' +
-          '<label class="build-input">Funding Target:</label><input type="text" ng-model="creator.target">' +
-           '{{ creator.target }}' +
-        '</li>' +
-        '<li>' +
-          '<label class="build-input">Project Title:</label><input ng-model="creator.title">' +
-            '{{ creator.title }}' +
-        '</li>' +
-        '<li>' +
-          '<label class="build-input">Short Blurb:</label><input type="text" ng-model="creator.blurb">' +
-            '{{ creator.blurb }}' +
-        '</li>' +
-        '<li>' +
-          '<label class="build-input">Location:</label><input ng-model="creator.location">' +
-            '{{ creator.location }}' +
-        '</li>' +
-        '<li>' +
-          '<label class="build-input">Category:</label><input type="text" ng-model="creator.category">' +
-            '{{ creator.category }}' +
-        '</li>' +
-        '<li>' +
-          '<label class="build-input">Campaign Start Date:</label><input ng-model="creator.startDate">' +
-            '{{ creator.startDate }}' +
-        '</li>' +
-        '<li>' +
-          '<label class="build-input">Campaign End Date:</label><input type="text" ng-model="creator.endDate">' +
-            '{{ creator.endDate }}' +
-        '</li>' +
-      '</ul>' +
-    '</form>' +
-  '</div> <!-- close campaignCreator Controller -->' +
-'</div> <!-- close #create -->'
-  };
-});
-
-// test service, creates the method testingMyService
-legacy.service('data-service', function() {
-  this.testingMyService(function() {
-    console.log('This is my service!');
-  });
-});
 // injects homepage content into index.html
 var legacy = angular.module('legacyCrowdApp');
 legacy.controller('homepageCtrl', function() {
@@ -141,7 +140,7 @@ legacy.controller('homepageCtrl', function() {
 legacy.directive('navbar', function() {
   return {
     template: 
-    '<!-- BEGIN Navbar -->' +
+    '<!-- Navbar -->' +
     '<div id="nav-menu" class="navbar navbar-fixed-top navbar-inverse" role="navigation">' +
      '<div class="container-fluid" id="nav-container">' +
         '<div class="navbar-header">' +
@@ -185,7 +184,7 @@ legacy.directive('header', function() {
       '<br>' +
       '<br>' +
       '<button id="learn-more">Learn More</button>' +
-      '<button id="create-account">Create Account</button>' +
+      '<button id="create-account" data-toggle="modal" data-target="#login-modal">Create Account</button>' +
       '<br>' +
     '</div> <!-- close #header -->'
   };
@@ -218,5 +217,116 @@ and handling user input campaign data within the $scope */
 legacy.controller('loginCtrl', function($scope) {
   $scope.helloWorld = function() {
     console.log("testing - helloWorld function within the loginCtrl");
+  };
+});
+
+legacy.directive('loginModal', function() {
+  return {
+    template:
+      '<!-- Login Modal -->' +
+      '<div id="login">' +
+        '<div id="login-modal" class="modal fade" tabindex="-1" role="dialog" style="display: none;">' +
+          '<div class="modal-dialog">' +
+            '<div class="modal-content">' +
+              '<div class="modal-header" align="center">' +
+                '<img id="login-logo" src="../../images/legacy-crowd-logo.png">' +
+                '<button type="button" class="close" data-dismiss="modal">' +
+                  '<span class="glyphicon glyphicon-remove"></span>' +
+                '</button>' +
+              '</div>' +
+              '<div id="login-container">' +
+              '<login-form>' +
+              '</login-form>' +
+              '<lost-password>' +
+              '</lost-password>' +
+              '<register>' +
+              '</register>' +
+            '</div> <!-- close .modal-content -->' +
+          '</div> <!-- close .modal-dialogue -->' +
+        '</div> <!-- close #login-modal -->' +
+      '</div> <!-- close #login -->'
+  };
+});
+
+legacy.directive('loginForm', function() {
+  return {
+    template: 
+      '<!-- Login Form -->' +
+        '<form id="login-form">' +
+          '<div class="modal-body">' +
+            '<div id="div-login-msg"> <!-- success/fail msg -->' +
+              '<div id="icon-login-msg" class="glyphicon glyphicon-chevron-right"></div>' +
+              '<span id="text-login-msg">Type your username and password.</span>' +
+            '</div>' +
+            '<input id="login-username" class="form-control" type="text" placeholder="Username" required>' +
+            '<input id="login-password" class="form-control" type="password" placeholder="Password" required>' +
+            '<div class="checkbox">' +
+              '<label>' + 
+                '<input type="checkbox">Remember me</input>' +
+              '</label>' + 
+            '</div>' + 
+          '</div>' + 
+          '<div class="modal-footer">' + 
+            '<div>' + 
+              '<button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>' + 
+            '</div>' + 
+            '<div>' + 
+              '<button id="login-lost-btn" type="button" class="btn btn-link">Lost Password?</button>' + 
+              '<button id="login-register-btn" type="button" class="btn btn-link">Register</button>' + 
+            '</div>' + 
+          '</div> <!-- close .modal-footer -->' + 
+        '</form> <!-- close #login-form -->'
+  };
+});
+
+legacy.directive('lostPassword', function() {
+  return {
+    template: 
+      '<!-- Lost Password Form -->' +
+        '<form id="lost-form" style="display:none;">' +
+          '<div class="modal-body">' +
+            '<div id="div-lost-msg"> <!-- success/fail msg -->' +
+              '<div id="icon-lost-msg" class="glyphicon glyphicon-chevron-right"></div>' +
+              '<span id="text-lost-msg">Enter your e-mail to recover your password.</span>' +
+            '</div>' +
+            '<input id="lost-email" class="form-control" type="text" placeholder="e-mail" required>' +
+          '</div>' +
+          '<div class="modal-footer">' +
+            '<div>' +
+             '<button type="submit" class="btn btn-primary btn-lg btn-block">Send</button>' +
+            '</div>' +
+            '<div>' +
+              '<button id="lost-login-btn" type="button" class="btn btn-link">Log In</button>' +
+              '<button id="lost-register-btn" type="button" class="btn btn-link">Register</button>' +
+            '</div>' +
+          '</div>' +
+        '</form> <!-- close #lost-form -->'
+  };
+});
+
+legacy.directive('register', function() {
+  return {
+    template: 
+      '<!-- Register Form -->' +
+        '<form id="register-form" style="display:none;">' +
+          '<div class="modal-body">' +
+            '<div id="div-register-msg"> <!-- success/fail msg -->' +
+              '<div id="icon-register-msg" class="glyphicon glyphicon-chevron-right"></div>' +
+              '<span id="text-register-msg">Register an account.</span>' +
+            '</div>' +
+            '<input id="register-username" class="form-control" type="text" placeholder="Username" required>' +
+            '<input id="register-email" class="form-control" type="text" placeholder="E-Mail" required>' +
+            '<input id="register-password" class="form-control" type="password" placeholder="Password" required>' +
+          '</div>' +
+          '<div class="modal-footer">' +
+            '<div>' +
+              '<button type="submit" class="btn btn-primary btn-lg btn-block">Register</button>' +
+            '</div>' +
+            '<div>' +
+              '<button id="register-login-btn" type="button" class="btn btn-link">Log In</button>' +
+              '<button id="register-lost-btn" type="button" class="btn btn-link">Lost Password?</button>' +
+            '</div>' +
+          '</div>' +
+        '</form> <!-- close #register-form -->'
   };
 });
